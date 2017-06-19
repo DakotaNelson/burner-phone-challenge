c26b8ae945f30b31f9ab810bea01bb03b8ebf0cb
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-06-19 18:45:17 +0300

    first pass - it runs, no idea if numbers make sense

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 Can you identify a burner phone from a stream of (fake) AT&amp;T Hemisphere data?
 
 Inspired by Kenneth Lipp on [Twitter](https://twitter.com/kennethlipp/status/848565438061654017).
+
+Specifically, this passage was interesting to me:
+
+> Let's say a narcotics task force is trying to maintain a wiretap on a drug dealer, but the drug dealer uses prepaid "burner" phones and is constantly changing his number.
+> However, from each new phone number, this drug dealer makes calls to may of the same people - his mom, girlfriend, a supplier - and also makes most of his calls from a limited range of locations. Because AT&T can analyze so many call-detail records with such well-developed custom software, it can easily track these patterns to infer which unknown number is really the suspect. Since it can do this in real time by mining its live data stream, by the time the suspect burns an old phone and makes a few calls from his new one, he's back in the net.
+
+AT&T can do this. Can you?
+
+First though, you need some data to play with. So I made some up. It's definitely not accurate, because I don't have any AT&T data to work from, but it should be close enough to give you an idea of what AT&T (and law enforcement, by extension) can do. If you have ideas for how to make it more accurate, or some legitimate (anonymized, please!) data to build a generative model with, that would be neat and you should [let me know](https://strikersecurity.com/contact/).
+
+
+And so,
+
+The Rules
+=========
+Coming soon
+
+
+Big Todos
+=========
+- [] make locations/contacts called non-uniformly (i.e call some friends more than others, be in some places more than others)
---
+++ src/generate.py
@@ -0,0 +1,158 @@
+import random
+import math
+import uuid
+
+### ASSUMPTIONS (which should change, TODO) ###
+# 1. Everyone only has one phone at a time
+# 2. Highest resolution is an hour, only one "call" can happen per hour
+# 3. No day/night cycle
+
+### Parameters ###
+
+# total number of cells for the area of interest
+num_cells = 1000
+
+# total number of people call records are collected for
+num_people = 1000
+
+# time is in hours, starting from 0
+# so, if end time is 48, the generated data spans two days
+end_time = 30 * 24
+
+# on average, people change numbers every... (in hours)
+change_numbers_every = 365 * 24 * 3
+
+people = []
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+
+
+class Person():
+    """ a single person, who has one or more cell phones, each with a unique
+    cell phone number """
+    def __init__(self):
+        # how can we uniquely identify them?
+        self._uuid = uuid.uuid4()
+        # NOTE using this would be cheating :)
+
+        # number of contacts they usually call
+        self.num_contacts = math.ceil(random.normalvariate(10, 3))
+        self.num_contacts = clamp(self.num_contacts, 1, 200)
+
+        # who do they usually call?
+        self.contacts = []
+
+        # how habitual are they in their contacts?
+        # 1 = always contacts the same set of contacts
+        # 0 = completely uniform random contact for each call
+        self.contact_regularity = random.normalvariate(.95, .01)
+
+        # how often do they call people?
+        # mean of 3 = 1/3 chance for average person to make call any given hour
+        self.cell_usage = random.normalvariate(3,.30)
+        if self.cell_usage < 0:
+            self.cell_usage = .001
+
+        # number of locations they usually call from
+        self.num_cells = math.ceil(random.normalvariate(10, 3))
+        self.num_cells = clamp(self.num_cells, 1, 200)
+
+        # what cells (locations) do they commonly call from?
+        self.cells = random.sample(range(num_cells), self.num_cells)
+
+        # how habitual are they in their locations?
+        # 1 = always uses the same set of locations
+        # 0 = completely uniform random cell for each call
+        self.cell_regularity = random.normalvariate(.8, .05)
+
+        # how frequently (on average) does someone change phones?
+        self.number_switch_frequency = 0
+        while self.number_switch_frequency < 24:
+            # stipulate that nobody change numbers more than once per day
+            frequency = 1.0/change_numbers_every
+            self.number_switch_frequency = random.expovariate(frequency)
+
+        prob_per_hour = 1/self.number_switch_frequency
+        # everybody "switches" at t=0
+        self.switch_times = [0]
+        for hour in range(end_time):
+            if random.random() < prob_per_hour:
+                self.switch_times.append(hour)
+
+        self.numbers = []
+        for time in self.switch_times:
+            self.numbers.append(CellNumber(self))
+
+        assert len(self.switch_times) == len(self.numbers)
+
+    def pick_contacts(self, people):
+        """ given a full list of people, pick who this person
+        usually calls """
+        self.contacts = random.sample(people, self.num_contacts)
+
+    def __repr__(self):
+        return "<Person contacts:{} cells:{} switch_frequency:{}" \
+               "switch_times:{}>".format(
+                  len(self.contacts),
+                  len(self.cells),
+                  self.number_switch_frequency,
+                  len(self.switch_times)
+                  )
+
+
+class CellNumber():
+    """ Describes a single cell number, which (usually) has a number of call
+    records associated with it. A person could have one number, or more """
+
+    def __init__(self, person):
+        self.uuid = uuid.uuid4()
+        self.person = person
+        self.call_records = []
+
+    def generateRecords(self, people):
+        for hour in range(end_time):
+            if random.random() < 1/self.person.cell_usage:
+                # they made a call/texted/etc. in this hour
+                self.generateRecord(hour, people)
+
+    def generateRecord(self, time, people):
+        """ generates a call record from a random location to a random contact
+        at a specified time """
+
+        # choose a location
+        if random.random() < self.person.cell_regularity:
+            # in one of their cells
+            location = random.choice(self.person.cells)
+        else:
+            # outside of one of their usual cells
+            location = random.randrange(num_cells)
+
+        # choose a contact
+        if random.random() < self.person.contact_regularity:
+            # calling a regular contact
+            contact = random.choice(self.person.contacts)
+        else:
+            # venturing to call someone totally new
+            contact = random.choice(people)
+
+        # assemble call record, store in self.call_records
+        self.call_records.append({'cell':location, 'contact': contact, 'time': time})
+
+
+if __name__ == "__main__":
+    for x in range(num_people):
+        people.append(Person())
+
+    for person in people:
+        person.pick_contacts(people)
+
+        for cell_number in person.numbers:
+            cell_number.generateRecords(people)
+
+    # print how many call records each person has
+    print([p for p in people])
+    print([sum([len(x.call_records) for x in p.numbers]) for p in people])
+
+    # TODO shuffle/sort by time and export records
+
