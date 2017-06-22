eb0d008799f01a4d7f2fe71419df158d5f64fe38
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-06-22 10:40:48 +0800

    write out solutions mapping numbers to people

--- src/generate.py
+++ src/generate.py
@@ -150,30 +150,30 @@ if __name__ == "__main__":
     for x in range(num_people):
         people.append(Person())
 
+    # build a list of all cell numbers
     cell_numbers = []
     for person in people:
         for cell_number in person.numbers:
             cell_numbers.append(cell_number)
 
+    # pick each person's favored contacts from the numbers
     for person in people:
         person.pick_contacts(cell_numbers)
 
+    # generate call records for each number
     for cell_number in cell_numbers:
         cell_number.generateRecords(cell_numbers)
 
-    # print how many call records each person has
-    # print([p for p in people])
-    # print([sum([len(x.call_records) for x in p.numbers]) for p in people])
-
+    # put all of the call records in one place
     all_records = []
     for person in people:
         for number in person.numbers:
             all_records.extend(number.call_records)
 
+    # sort the call records by time
     all_records = sorted(all_records, key=lambda k: k['time'])
 
-    # print(all_records)
-
+    # write out all of the call records
     with open('call_records.csv', 'w') as csvfile:
         fieldnames = ['time', 'cell', 'from', 'to']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -183,6 +183,28 @@ if __name__ == "__main__":
             writer.writerow(row)
 
     print("[*] Call record data written to call_records.csv")
+
+    # build a dict mapping numbers to people (the solutions!)
+    solutions = []
+    for person in people:
+        for number,time in zip(person.numbers, person.switch_times):
+            solutions.append({
+                'person': person._uuid,
+                'number': number.uuid,
+                'switch_time': time
+                })
+
+
+    # write out the "who has a number when" solutions
+    with open('solutions.csv', 'w') as csvfile:
+        fieldnames = ['person', 'number', 'switch_time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for row in solutions:
+            writer.writerow(row)
+
+    print("[*] Solutions written to solutions.csv")
 
     try:
         import networkx as nx
